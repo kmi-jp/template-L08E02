bd45d66092bd6795e0fd9310361e08a0f5857411
Tomáš Mikula <mail@tomasmikula.cz>
2022-07-24 15:50:28 +0200

    Fix

--- tests/L07E02/test_series_L07E02.py
+++ tests/L07E02/test_series_L07E02.py
@@ -25,3 +25,24 @@ def test_series_get_item(salaries):
 def test_iteration(salaries):
     assert isinstance(salaries.__iter__(), types.GeneratorType)
     assert sum([value for value in salaries]) == sum(salaries.values)
+
+
+def test_items(users, salaries):
+    labels = []
+    values = []
+    for label, value in salaries.items():
+        labels.append(label)
+        values.append(value)
+
+    assert isinstance(salaries.items(), zip)
+
+    assert labels == users.labels
+    assert values == salaries.values
+
+
+@pytest.mark.parametrize(
+    "function",
+    [Series, Series.get, Series.from_csv, Series.shape, Series.items],
+)
+def test_docstrings(function):
+    assert function.__doc__ is not None
